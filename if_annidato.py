568f5d11f47df6a972a4cd84c3aaea0548df5b50
massiprofessor <massiprofessor@gmail.com>
2025-03-07 12:05:41 +0100

    if annidato

---
+++ if_annidato.py
@@ -0,0 +1,13 @@
+numero=int(input("Inserisci numero: "))
+if numero == 5:
+    print("Hai indovinato il numero")
+    nome= input("Inserisci il nome: ")
+    if nome === "massimo":
+        print("Sei bravo")
+    else:
+        print("Non sei bravo")
+else:
+    print("Non hai indovinato il numero")
+
+
+    
