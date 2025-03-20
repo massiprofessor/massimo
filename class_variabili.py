466be3348c98f94c5a5f0f63a618a6dd4a778278
massiprofessor <massi.professor@gmail.com>
2025-03-20 10:04:14 +0100

    -

---
+++ class_variabili.py
@@ -0,0 +1,13 @@
+class anna:
+    
+    def __init__(self):
+        self.a=0
+        self.b=6
+    
+    def prova(self,h):
+        a= h*2 + self.b
+        return a
+
+
+anna= anna()  
+print(anna.prova(6))
