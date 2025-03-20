255c2fc5eb2c28df2a896f0c0b32dc50f74e813b
massiprofessor <massi.professor@gmail.com>
2025-03-20 10:09:43 +0100

    -

---
+++ modulos.py
@@ -0,0 +1,14 @@
+def somma(a,b):
+    r=a+b
+    return r
+
+
+def potenza(a,b):
+    r= a**b
+    return r
+
+
+if __name__ == "__main__":
+    print("Questo è il programma principale")
+else:
+    print("Questo è un modulo")
